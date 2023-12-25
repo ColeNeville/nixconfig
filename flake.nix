7a2bf9dbf45fdfca69ab2fac50841d5c5b67a68b
ColeNeville <colewneville2@gmail.com>
2023-12-24 21:33:15 -0700

    Update to NixOS 23.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Cole's NixOS Config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-master.url = "github:NixOS/nixpkgs/master";
 
