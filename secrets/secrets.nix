bab58b343ccf955b4695a902397e470e6e89a5d1
ColeNeville <colewneville2@gmail.com>
2024-01-04 17:20:03 -0700

    Updating the secrets for alexander-2

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -6,25 +6,21 @@ in {
   "telegraf-alexander-1.env.age".publicKeys = [
     users."cole@garuda"
     hosts.alexander-1.ed25519
-    hosts.alexander-1.rsa
   ];
 
   "telegraf-alexander-2.env.age".publicKeys = [
     users."cole@garuda"
     hosts.alexander-2.ed25519
-    hosts.alexander-2.rsa
   ];
 
   "telegraf-alexander-3.env.age".publicKeys = [
     users."cole@garuda"
     hosts.alexander-3.ed25519
-    hosts.alexander-3.rsa
   ];
 
   "telegraf-alexander-4.env.age".publicKeys = [
     users."cole@garuda"
     hosts.alexander-4.ed25519
-    hosts.alexander-4.rsa
   ];
 
   # "github-token.age".publicKeys = [
@@ -36,16 +32,14 @@ in {
   "k3s.env.age".publicKeys = [
     users."cole@garuda"
     hosts.alexander-1.ed25519
-    hosts.alexander-1.rsa
+    hosts.alexander-2.ed25519
     hosts.alexander-4.ed25519
-    hosts.alexander-4.rsa
   ];
 
   "k3s-token.age".publicKeys = [
     users."cole@garuda"
     hosts.alexander-1.ed25519
-    hosts.alexander-1.rsa
+    hosts.alexander-2.ed25519
     hosts.alexander-4.ed25519
-    hosts.alexander-4.rsa
   ];
 }
